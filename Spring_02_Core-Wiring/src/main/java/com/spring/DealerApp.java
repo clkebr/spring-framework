080da70b7db925ac4acea17512f763817d1d7b12
ebr <clkebr2601@gmail.com>
2022-08-24 20:24:06 +0200

    wiring is done

---
+++ Spring_02_Core-Wiring/src/main/java/com/spring/DealerApp.java
@@ -0,0 +1,16 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class DealerApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(ConfigCar.class);
+        Car car = context.getBean(Car.class);
+        Person person = context.getBean(Person.class);
+
+        System.out.println("Person's name : " + person.getName());
+        System.out.println("Car's make : " + car.getMake());
+        System.out.println("Person's car : " + person.getCar().getMake());
+    }
+}
