329c947c9a98a96909f2215467d66f17af912858
ebr <clkebr2601@gmail.com>
2022-10-20 11:38:33 +0200

    db table created with hibernate

---
+++ Spring_10_ORM-TableCreation/src/main/java/com/spring/entity/Student.java
@@ -0,0 +1,13 @@
+package com.spring.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+}
