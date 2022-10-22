8b6aa87b047bf4cb49bc0eaf89b00dfdb6f2e052
ebr <clkebr2601@gmail.com>
2022-10-22 16:42:02 +0200

    task

---
+++ Spring_11_ORM-Mapping_OneToOne/src/main/java/com/spring/entity/Department.java
@@ -0,0 +1,26 @@
+package com.spring.entity;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "departments")
+@Data
+@NoArgsConstructor
+public class Department extends BaseEntity {
+    @Column(length = 100)
+    private String department;
+    @Column(length = 100)
+    private String division;
+
+    public Department(String department, String division) {
+        this.department = department;
+        this.division = division;
+    }
+}
---
+++ Spring_11_ORM-Mapping_OneToOne/src/main/java/com/spring/entity/Employee.java
@@ -0,0 +1,39 @@
+package com.spring.entity;
+
+import com.spring.enums.Gender;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employees")
+@Data
+@NoArgsConstructor
+public class Employee extends BaseEntity{
+    @Column(length = 50)
+    private String firstName;
+    @Column(length = 50)
+    private String lastName;
+    @Column(length = 50)
+    private String email;
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+//    @Column(length = 17)
+//    private String department;
+    @Enumerated(EnumType.ORDINAL)
+    private Gender gender;
+    private  int salary;
+//    private  int regionId;
+
+
+    public Employee(String firstName, String lastName, String email, LocalDate hireDate, int salary,Gender gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.hireDate = hireDate;
+        this.gender = gender;
+        this.salary = salary;
+    }
+}
