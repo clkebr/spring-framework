63e5131612cadd29442420b1050558de38eee39d
ebr <clkebr2601@gmail.com>
2022-11-12 14:11:02 +0100

    ticket & movie Queries are completed

--- Spring_13_ORM-CinemaLab/src/main/java/com/spring/repository/TicketRepository.java
+++ Spring_13_ORM-CinemaLab/src/main/java/com/spring/repository/TicketRepository.java
@@ -2,25 +2,38 @@ package com.spring.repository;
 
 import com.spring.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to count how many tickets a user bought
+    Integer countAllByUserAccountId(Long id);
 
     //Write a derived query to list all tickets by specific email
+    List<Ticket> findByUserAccountEmail(String email);
 
     //Write a derived query to count how many tickets are sold for a specific movie
 
+    Integer countAllByMovieCinemaMovieName(String name);
+
     //Write a derived query to list all tickets between a range of dates
 
+    List<Ticket> findAllByDateTimeBetween(LocalDateTime dateTime1, LocalDateTime dateTime2);
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query that returns all tickets are bought from a specific user
+    @Query("SELECT t FROM Ticket t WHERE t.userAccount.id = ?1")
+    List<Ticket> finAllByUser(@Param("userId") Long id);
 
     //Write a JPQL query that returns all tickets between a range of dates
-
+    @Query("SELECT t FROM Ticket t WHERE t.dateTime BETWEEN ?1 AND ?2")
+    List<Ticket> finAllByDateBetween(@Param("date1") LocalDateTime dateTime1, @Param("date2")LocalDateTime dateTime2);
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to count the number of tickets a user bought
