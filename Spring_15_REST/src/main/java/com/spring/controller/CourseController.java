7881d70241652c9e34990dfe3bc1140330c40c1d
ebr <clkebr2601@gmail.com>
2022-12-14 20:35:17 +0100

    Get /courses REST api is created

---
+++ Spring_15_REST/src/main/java/com/spring/controller/CourseController.java
@@ -0,0 +1,28 @@
+package com.spring.controller;
+
+import com.spring.dto.CourseDTO;
+import com.spring.service.CourseService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public List<CourseDTO> getAllCourses(){
+
+        List<CourseDTO> courses = courseService.getCourses();
+        return courses;
+    }
+}
---
+++ Spring_15_REST/src/main/java/com/spring/repository/CourseRepository.java
@@ -0,0 +1,12 @@
+package com.spring.repository;
+
+import com.spring.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findAllByCategory(String category);
+
+}
