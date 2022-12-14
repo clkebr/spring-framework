77b285abfe6f9447f9bfefedd4937194921fd104
ebr <clkebr2601@gmail.com>
2022-12-14 21:48:01 +0100

    Rest v2

--- Spring_15_REST/src/main/java/com/spring/controller/CourseController.java
+++ Spring_15_REST/src/main/java/com/spring/controller/CourseController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/courses")
+@RequestMapping("/courses/api/v1")
 public class CourseController {
     private final CourseService courseService;
 
